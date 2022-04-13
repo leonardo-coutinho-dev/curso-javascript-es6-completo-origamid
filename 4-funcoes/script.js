d7f2dce96e94014c7405d429aa3ab7d64ac9ea53
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-04-13 00:38:05 -0300

    functions

---
+++ 4-funcoes/script.js
@@ -0,0 +1,10 @@
+/*
+  functions are blocks of code that can be executed and reused
+  values can be passed through this blocks and the function will return another value
+*/
+
+function squareArea(side) {
+  return side * side;
+}
+
+console.log(squareArea(4));
